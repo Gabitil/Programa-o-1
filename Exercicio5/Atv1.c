9fcf6bb59b137294cdba4a275d0127beda8edd8a
Gabriel <61718293+Gabitil@users.noreply.github.com>
2022-10-20 11:12:17 -0300

    Create Atv1.c

---
+++ Exercicio5/Atv1.c
@@ -0,0 +1,14 @@
+/*Criar um programa para ler N notas de Lab. PC I e PC I. As notas devem ser de 0 a 100. Forneça N e a
+notas pelo teclado. Use uma repetição para garantir que as notas são de 0 a 100. Ao final apresente
+a média de cada disciplina. Gere o programa exer5_prog1.c.*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    
+    printf("hello");
+    return 0;
+}

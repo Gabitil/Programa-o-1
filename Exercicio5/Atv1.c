e341970a4b3d1076ba87b5931347d0edeb0d8742
Gabriel <61718293+Gabitil@users.noreply.github.com>
2022-10-23 10:31:15 -0300

    Feito e Funcionando

--- Exercicio5/Atv1.c
+++ Exercicio5/Atv1.c
@@ -8,6 +8,27 @@ a média de cada disciplina. Gere o programa exer5_prog1.c.*/
 
 int main()
 {
+    int num; 
+    printf("programa para ler n notas de 2 materias:\n");
+
+    printf("Digite a quantidade de notas a ser recebido: ");
+    scanf("%d", &num);
+
+    int nota1[num], nota2[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("\nDigite a nota n %d de lab.pc1 e pc1: ", i+1);
+        scanf("%d %d", &nota1[i], &nota2[i]);
+
+        while (nota1[i] <= 0 || nota1[i] >=100 || nota2[i] <=0 || nota2[i] >=100)
+        {
+            printf("\nDigite novamente o valor das notas: ");
+            scanf("%d %d", &nota1[i], &nota2[i]);
+        }
+        
+        
+    }
     
-    printf("hello");
+
 }
